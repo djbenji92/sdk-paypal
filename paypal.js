38ee20540770e5f73d41fefc570daa323b1994b8
gaelgf <gael.graffion@gmail.com>
2016-07-07 02:11:34 +0200

    payment creation done

---
+++ paypal.js
@@ -0,0 +1,102 @@
+'use strict';
+
+const https = require('https');
+const querystring = require('querystring');
+
+function getToken(username, password) {
+
+	var options = {
+		host: 'api.sandbox.paypal.com',
+		path: '/v1/oauth2/token',
+		method: 'POST',
+		auth: username + ':' + password,
+		headers: {
+			'Accept': 'application/json',
+			'Accept-Language': 'en_US'
+		}
+	};
+
+	var promise = new Promise(
+    	function(resolve, reject) {
+    		var req = https.request(options, function(res) {
+				var response = '';
+				res.on('data', function(data) {
+					response += data;
+				});
+				res.on('end', function() {
+					response = JSON.parse(response);
+					resolve(response.access_token);
+				});
+			});
+
+			req.on('error', function(e) {
+			  reject('problem with request: ' + e.message);
+			});
+
+			req.write(querystring.stringify({'grant_type': 'client_credentials'}));
+			req.end();
+    	}
+	);
+
+	return promise;
+}
+
+function createPayment(username, password, return_url, cancel_url, transactions) {
+
+	return getToken(username, password)
+		.then(function(token) {
+			return create(token, return_url, cancel_url, transactions);
+		});
+
+	function create(token, return_url, cancel_url, transactions) {
+		var options = {
+			host: 'api.sandbox.paypal.com',
+			path: '/v1/payments/payment',
+			method: 'POST',
+			headers: {
+				Accept: 'application/json',
+				Authorization: 'Bearer ' + token,
+				'Content-Type': 'application/json'
+			}
+		};
+
+		var params = {
+			'intent': 'sale',
+			'redirect_urls':{
+				'return_url': return_url,
+				'cancel_url': cancel_url
+			},
+			'payer': {
+				'payment_method': 'paypal'
+			},
+			'transactions': transactions
+		};
+
+		var promise = new Promise(
+	    	function(resolve, reject) {
+	    		var req = https.request(options, function(res) {
+					var response = '';
+					res.on('data', function(data) {
+						response += data;
+					});
+					res.on('end', function() {
+						response = JSON.parse(response);
+						resolve(response);
+					});
+				});
+
+				req.on('error', function(e) {
+				  reject('problem with request: ' + e.message);
+				});
+
+				req.write(JSON.stringify(params));
+				req.end();
+	    	}
+		);
+
+		return promise;
+	}
+
+}
+
+exports.createPayment = createPayment;
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var app = require('express')(),
     server = require('http').createServer(app),
-    express = require('express')
+    express = require('express');
+
+const paypal = require('./paypal.js');
 
 app.use('/script', express.static(__dirname + '/script'));
 app.use('/views', express.static(__dirname + '/views'));
@@ -9,7 +11,25 @@ app.use('/views', express.static(__dirname + '/views'));
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
-console.log("Server start on port 8080...");
-console.log("Go coder les flemmards !");
 
-server.listen(8080);
+var transactions = [
+	{
+		amount: {
+			total: '1.00',
+			currency: 'EUR'
+		}
+	}
+];
+
+paypal.createPayment('AYjeNPR31RzSk33-YyPDWHiAyV-a5TCxXSOqzDD4Dh6_W0Pxx3RW_l5B2H66n_j5QKxrT88g1_QI8Dct',
+					 'EFnkSRUJp0_8jdTFqmAKrQL_yFhSu5Q09joh6XIA-RZNcxa_gtN_-GB4Xh6hEA7ijhwsOC-pTz32Azxg',
+					 'http://example.com/your_redirect_url.html',
+					 'http://example.com/your_cancel_url.html',
+					 transactions)
+	.then(function(response) {
+		console.log(response);
+	})
+	.catch(function(error) {
+		console.log(error);
+	});
+//server.listen(8080);
